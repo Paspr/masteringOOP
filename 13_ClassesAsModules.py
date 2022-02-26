8468b27890e8d493d32d5cd9aeed66d6ce5a7abc
Pavel Osminin <osperevod@gmail.com>
2022-02-27 01:28:39 +0500

    initial version

---
+++ 13_ClassesAsModules.py
@@ -0,0 +1,16 @@
+"""
+В Python возможны "классы как модули":
+1. Как импорт пакетов, например:
+import math
+math.ceil(x)
+
+2. Можно импортировать класс из другого файла, например:
+
+в menu.py импорт будет производиться так:
+from classes.window import RequiredClass
+
+menu.py
+classes/
+    window.py
+
+"""
